65b1e0d902d16abeb10504df8febb32254494282
sreedev <sreedev@localhost.localdomain>
2021-03-18 13:54:05 +0000

    store using php arrays

--- index.php
+++ index.php
@@ -2,34 +2,40 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ecommerce</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>phpecommercetraining</title>
 </head>
 <body>
-<div class="items">
-<?php 
-$fruitcollection=array[
-  array[
-    "name"=>"apple 1kg"
-    "image"=>"img/apple.jpg"
-    "price"=>"70"
-],
-  array[
-    "name"=>"orange 1kg"
-    "image"=>"img/orange.jpg"
-    "price"=>"60"
-],
-];
-foreach ($fruitcollection as $fruit){?>
-  <div class="item">
-    <img src="<?php echo $fruit["image"]?>" alt="">
-    <h1><?php echo $fruit["name"]?></h1>
-    <h4><?php echo $fruit["price"]?></h4>
-    <button>Add to cart</button>
-    <h1>hello</h1>
-  </div>
-<?php }
-?>
-</div>
+        <?php
+            $fruitcollection=array(
+                array(
+                    "name" => " apple 1 kg",
+                    "image"=> "img/apple.jpg",
+                    "price"=> "70 rs"
+
+                ),
+                array(
+                    "name" => " orange 1 kg",
+                    "image"=> "img/orange.jpg",
+                    "price"=> "60 rs"
+
+                )
+
+                );
+
+
+        foreach ($fruitcollection as $fruit) { ?>
+        <div class="item">
+            <div class="imagecontainer">
+                 <img src="<?php echo $fruit["image"] ?>" alt="">
+            </div>
+            <h3><?php echo $fruit["name"] ?></h3>
+            <h5><?php echo $fruit["price"] ?></h5>
+            <input type="button" value="Add To Cart">
+        </div>
+        <?php } 
+        ?>
 </body>
 </html>
